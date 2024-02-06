1e9783ad51df3d72fddf692fb0ff63a48ee65330
n377i <annettewoehler@gmx.de>
2024-02-06 19:31:14 +0100

    + Add styled components

--- components/RecipeList/index.js
+++ components/RecipeList/index.js
@@ -1,15 +1,51 @@
 import { recipes } from "@/lib/data";
+import styled from "styled-components";
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  padding: 1rem;
+`;
+
+const ImageContainer = styled.div`
+  width: 100%;
+  padding-top: 100%;
+  position: relative;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-radius: 8px;
+  box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.1);
+`;
+
+const Caption = styled.figcaption`
+  text-align: center;
+  margin-top: 0.25rem;
+`;
 
 export default function RecipeList() {
   return (
-    <div>
+    <GridContainer>
       {recipes.map((recipe) => (
         <div key={recipe.id}>
-          <h2>{recipe.name}</h2>
-          <img src={recipe.image} alt={recipe.name} />
+          <figure>
+            <ImageContainer>
+              <Image 
+                src={recipe.image} 
+                alt={recipe.name}
+              />
+            </ImageContainer>
+            <Caption>{recipe.name}</Caption>
+          </figure>
         </div>
       ))}
-    </div>
+    </GridContainer>
   );
-  å;
 }
