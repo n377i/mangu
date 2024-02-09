58b2cc8e6ccb6ad836780dfe2dd22593879691c8
Annette Wöhler <annettewoehler@gmx.de>
2024-02-09 22:07:13 +0100

    + Add TopNav

---
+++ components/TopNav/index.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Image from "next/image";
+import styled from "styled-components";
+
+const NavContainer = styled.nav`
+  position: fixed;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  padding: 50px 24px 10px;
+  width: 100%;
+  background: var(--color-white);
+  z-index: 1;
+`;
+
+export default function TopNav() {
+  return (
+    <>
+      <NavContainer>
+        <Link href="#">
+          <img src="/assets/icon_grid.svg" alt="Customize layout" />
+        </Link>
+        <img src="/assets/lettermark.svg" alt="Mangu lettermark" />
+        <Link href="#">
+          <img src="/assets/icon_settings.svg" alt="Settings" />
+        </Link>
+      </NavContainer>
+    </>
+  );
+}
