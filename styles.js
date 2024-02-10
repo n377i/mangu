ad52abfd64b31fc57eea2fa9b26ce0aaec2c0330
Annette Wöhler <annettewoehler@gmx.de>
2024-02-11 00:42:46 +0100

    + Add color and font styles

--- styles.js
+++ styles.js
@@ -9,13 +9,19 @@ export default createGlobalStyle`
   :root {
     /* Color styles */
     --color-white: #fff;
+    --color-grey: #666;
+    --color-light-grey: #cdc8bf;
     --color-night: #333;
+    --color-orange: #fea500;
     --color-gradient: linear-gradient(to bottom right, #fec600, #fe9900);
 
     /* Font styles */
     --font-family: ${inter.style.fontFamily};
-    --font-body: normal 400 1rem/1.25 var(--font-family);
-    --font-caption: normal 500 1rem/1.25 var(--font-family);
+    --font-body: 300 1rem/1.5 var(--font-family);
+    --font-caption: 500 1rem/1.25 var(--font-family);
+    --font-form-heading: 500 1.25rem/1.25 var(--font-family);
+    --font-form-label: 400 1.125rem/1.5 var(--font-family);
+    --font-button: 500 1.125rem/1.25 var(--font-family);
   }
 
   *,
