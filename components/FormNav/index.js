23b474dd284521fe6734bfdc0d416de5fe406ec6
Annette Wöhler <annettewoehler@gmx.de>
2024-02-11 00:43:47 +0100

    + Add FormNav component

---
+++ components/FormNav/index.js
@@ -0,0 +1,43 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+import styled from "styled-components";
+
+const NavContainer = styled.div`
+  padding: 55px 0 80px;
+  background: var(--color-gradient);
+`;
+
+const Nav = styled.nav`
+  position: fixed;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 24px;
+`;
+
+const FormHeading = styled.h1`
+  font: var(--font-form-heading);
+  color: var(--color-white);
+`;
+
+export default function FormNav() {
+  const router = useRouter();
+  const isAddForm = router.pathname === "/add-form";
+  const headingText = isAddForm ? "Rezept hinzufügen" : "Rezept bearbeiten";
+
+  return (
+    <>
+      <NavContainer>
+        <Nav>
+          <Link href="/">
+            <img src="/assets/icon_arrow-left.svg" alt="Go back" />
+          </Link>
+          <FormHeading>{headingText}</FormHeading>
+          <Link href="#">
+            <img src="/assets/icon_settings--white.svg" alt="Settings" />
+          </Link>
+        </Nav>
+      </NavContainer>
+    </>
+  );
+}
