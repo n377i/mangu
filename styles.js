1c734e24ec7bf2ea877f2d100d1e198b7616aae0
Annette Wöhler <annettewoehler@gmx.de>
2024-02-11 22:37:34 +0100

    + Add default link style

--- styles.js
+++ styles.js
@@ -32,6 +32,11 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+  a {
+    text-decoration: none;
+    color: var(--color-night);
+  }
+
   body {
     background: var(--color-white);
     font: var(--font-body);
