8dd9ba1f6126ede6ba9da96c4df3db43e034807c
Annette Wöhler <annettewoehler@gmx.de>
2024-02-11 22:40:44 +0100

    + Add Recipe Schema

---
+++ db/models/Recipe.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const recipeSchema = new Schema({
+  image: { type: String, required: true },
+  title: { type: String, required: true },
+  servings: { type: Number, required: true },
+  ingredients: { type: String, required: true },
+  preperation: { type: String, required: true },
+});
+
+const Recipe = mongoose.models.Recipe || mongoose.model("Recipe", recipeSchema);
+
+export default Recipe;
