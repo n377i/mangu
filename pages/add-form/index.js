94f6d93ed87be8a4ecf40322f916edff5a9f46c5
Annette Wöhler <annettewoehler@gmx.de>
2024-02-11 22:42:28 +0100

    * Update styled components

--- pages/add-form/index.js
+++ pages/add-form/index.js
@@ -1,23 +1,14 @@
 import AddForm from "@/components/AddForm";
 import FormNav from "@/components/FormNav";
-import styled from "styled-components";
-
-const FormContainer = styled.div`
-  position: relative;
-  top: -36px;
-  padding: 45px 24px 14px;
-  background: var(--color-white);
-  border-radius: 40px;
-  z-index: 1;
-`;
+import { Card } from "@/components/Layout/Card";
 
 export default function AddFormPage() {
   return (
     <>
       <FormNav />
-      <FormContainer>
+      <Card>
         <AddForm />
-      </FormContainer>
+      </Card>
     </>
   );
 }
