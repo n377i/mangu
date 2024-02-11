0df7bc26031adc32ecdea94990b338b7f7bd0d5f
Annette Wöhler <annettewoehler@gmx.de>
2024-02-11 22:37:51 +0100

    + Add Card component

---
+++ components/Layout/Card/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  position: relative;
+  top: -36px;
+  padding: 45px 24px 14px;
+  background: var(--color-white);
+  border-radius: 40px;
+  z-index: 1;
+`;
