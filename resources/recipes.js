0695a75b8d5d97e347dbd451f40ebc49bd9b04d6
Annette Wöhler <annettewoehler@gmx.de>
2024-02-15 17:28:28 +0100

    + Add mock data array

---
+++ resources/recipes.js
@@ -0,0 +1,62 @@
+[
+  {
+    _id: {
+      $oid: "626a48a8916f633a190f0251",
+    },
+    image: "/images/placeholder_dish.jpg",
+    title: "Spaghetti Bolognese",
+    servings: 2,
+    ingredients: "Lorem ipsum dolor sit amet",
+    preparation: "Stet clita kasd gubergren, no sea takimata sanctus est.",
+  },
+  {
+    _id: {
+      $oid: "626a48ef916f633a190f0252",
+    },
+    image: "/images/placeholder_dish.jpg",
+    title: "Mediterrane Gemüsepfanne",
+    servings: 2,
+    ingredients: "Lorem ipsum dolor sit amet",
+    preparation: "Stet clita kasd gubergren, no sea takimata sanctus est.",
+  },
+  {
+    _id: {
+      $oid: "626a4929916f633a190f0253",
+    },
+    image: "/images/placeholder_dish.jpg",
+    title: "Hähnchenfilet mit Kartoffelstampf",
+    servings: 2,
+    ingredients: "Lorem ipsum dolor sit amet",
+    preparation: "Stet clita kasd gubergren, no sea takimata sanctus est.",
+  },
+  {
+    _id: {
+      $oid: "626a494c916f633a190f0254",
+    },
+    image: "/images/placeholder_dish.jpg",
+    title: "Gebratener Lachs mit Kräuter-Soße",
+    servings: 2,
+    ingredients: "Lorem ipsum dolor sit amet",
+    preparation: "Stet clita kasd gubergren, no sea takimata sanctus est.",
+  },
+  {
+    _id: {
+      $oid: "626a49ce916f633a190f0255",
+    },
+    image: "/images/placeholder_dish.jpg",
+    title: "Vegetarische Quinoa-Salat-Bowl",
+    servings: 2,
+    ingredients: "Lorem ipsum dolor sit amet",
+    preparation: "Stet clita kasd gubergren, no sea takimata sanctus est.",
+  },
+  {
+    _id: {
+      $oid: "626a49f6916f633a190f0266",
+    },
+    image: "/images/placeholder_dish.jpg",
+    title: "Chili sin Carne mit Avocado-Dip",
+    servings: 4,
+    ingredients: "Lorem ipsum dolor sit amet",
+    preparation: "Stet clita kasd gubergren, no sea takimata sanctus est.",
+  },
+];
