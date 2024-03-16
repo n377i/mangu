992b2e5e183eab3ad8f48dd27daa2aad44b81e1d
Annette Wöhler <annettewoehler@gmx.de>
2024-03-16 12:07:40 +0100

    + Add intro animation

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,38 @@
+import { useState, useEffect, createContext } from "react";
+import { useRouter } from "next/router";
 import { SWRConfig } from "swr";
 import GlobalStyle from "../styles";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
-export default function App({ Component, pageProps }) {
+export const CoverContext = createContext();
+
+export default function MyApp({ Component, pageProps }) {
+  const [showCover, setShowCover] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const isReloaded =
+      typeof window !== "undefined" &&
+      window.performance &&
+      window.performance.navigation.type ===
+        window.performance.navigation.TYPE_RELOAD;
+
+    if (isReloaded && router.pathname === "/") {
+      setShowCover(true);
+      const timer = setTimeout(() => {
+        setShowCover(false);
+      }, 1500);
+      return () => clearTimeout(timer);
+    }
+  }, []);
+
   return (
-    <>
+    <CoverContext.Provider value={showCover}>
       <GlobalStyle />
       <SWRConfig value={{ fetcher }}>
         <Component {...pageProps} />
       </SWRConfig>
-    </>
+    </CoverContext.Provider>
   );
 }
