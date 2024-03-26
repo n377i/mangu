eacc84fad0ceec7be297cff36291c316d7fdc83d
Annette Wöhler <annettewoehler@gmx.de>
2024-03-26 20:26:27 +0100

    + Add image schema

--- db/models/Recipe.js
+++ db/models/Recipe.js
@@ -2,11 +2,16 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const recipeSchema = new Schema({
-  image: { type: String, required: false },
   title: { type: String, required: true },
   servings: { type: Number, required: true },
   ingredients: { type: String, required: true },
   preparation: { type: String, required: true },
+  image: {
+    type: new Schema({
+      url: String,
+      publicId: String,
+    }),
+  },
 });
 
 const Recipe = mongoose.models.Recipe || mongoose.model("Recipe", recipeSchema);
