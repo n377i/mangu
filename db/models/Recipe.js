35c1197f63cda9cb249df0b0c14b731f6e00783e
Annette Wöhler <annettewoehler@gmx.de>
2024-04-12 21:53:17 +0200

    * Set preparation field to not required

--- db/models/Recipe.js
+++ db/models/Recipe.js
@@ -6,7 +6,7 @@ const recipeSchema = new Schema({
   title: { type: String, required: true },
   servings: { type: Number, required: true },
   ingredients: { type: String, required: true },
-  preparation: { type: String, required: true },
+  preparation: { type: String, required: false },
 });
 
 const Recipe = mongoose.models.Recipe || mongoose.model("Recipe", recipeSchema);
