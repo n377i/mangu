7522ca11cb600344b6e2e6e0ad72ebb15988eebb
Annette Wöhler <annettewoehler@gmx.de>
2024-04-21 13:50:15 +0200

    * Update DetailsPage

--- pages/recipes/[id]/index.js
+++ pages/recipes/[id]/index.js
@@ -1,34 +1,19 @@
 import useSWR from "swr";
 import { useRouter } from "next/router";
-import { Card } from "@/styles";
 import { CenterDiv } from "@/styles";
 import { PuffLoader } from "react-spinners";
-import styled from "styled-components";
-import BackButton from "@/components/BackButton/BackButton";
-import EditButton from "@/components/EditButton/EditButton";
-import DeleteButton from "@/components/DeleteButton/DeleteButton";
-
-const ImageContainer = styled.div`
-  width: 100%;
-  height: 340px;
-`;
-
-const Image = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
+import RecipeDetails from "@/components/RecipeDetails/RecipeDetails";
 
 export default function DetailsPage() {
   const router = useRouter();
   const { isReady } = router;
   const { id } = router.query;
 
-  if (!id) {
-    return <p>ID not found</p>;
-  }
-
-  const { data: recipe, isLoading, error } = useSWR(`/api/recipes/${id}`);
+  const {
+    data: recipe,
+    isLoading,
+    error,
+  } = useSWR(id ? `/api/recipes/${id}` : null);
 
   if (!isReady || isLoading || error)
     return (
@@ -44,24 +29,5 @@ export default function DetailsPage() {
     router.push("/");
   }
 
-  return (
-    <>
-      <ImageContainer>
-        <BackButton />
-        <EditButton id={id} />
-        <DeleteButton deleteRecipe={deleteRecipe} />
-        <Image src={`${recipe.image}`} alt={recipe.title} />
-      </ImageContainer>
-      <Card>
-        <h1>{recipe.title}</h1>
-        <h2>Zutaten</h2>
-        <p>
-          Für <strong>{recipe.servings}</strong> Portionen
-        </p>
-        <p>{recipe.ingredients}</p>
-        <h2>Zubereitung</h2>
-        <p>{recipe.preparation}</p>
-      </Card>
-    </>
-  );
+  return <RecipeDetails recipe={recipe} id={id} deleteRecipe={deleteRecipe} />;
 }
