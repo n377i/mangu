2e14498668cdd68c99352bccf70553b72fe7951d
Annette Wöhler <annettewoehler@gmx.de>
2024-04-28 00:01:17 +0200

    + Add theme toggle functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,24 @@
 import { useState, useEffect, createContext } from "react";
 import { useRouter } from "next/router";
 import { SWRConfig } from "swr";
+import { PuffLoader } from "react-spinners";
+import { CenterDiv } from "@/styles";
 import GlobalStyle from "../styles";
+import useLocalStorage from "use-local-storage";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export const CoverContext = createContext();
 
 export default function MyApp({ Component, pageProps }) {
-  const [showCover, setShowCover] = useState(false);
   const router = useRouter();
+  const [showCover, setShowCover] = useState(false);
+  const [themeLoaded, setThemeLoaded] = useState(false);
+  const [theme, setTheme] = useLocalStorage("theme", "light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
 
   useEffect(() => {
     const isReloaded =
@@ -25,13 +34,41 @@ export default function MyApp({ Component, pageProps }) {
       }, 1500);
       return () => clearTimeout(timer);
     }
-  }, []);
+  }, [router.pathname]);
+
+  useEffect(() => {
+    const loadThemeFromStorage = () => {
+      const loadedTheme = JSON.parse(localStorage.getItem("theme")) || "light";
+      setTheme(loadedTheme);
+      setThemeLoaded(true);
+    };
+
+    if (!themeLoaded) {
+      loadThemeFromStorage();
+    }
+  }, [themeLoaded, setTheme]);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [theme]);
+
+  if (!themeLoaded) {
+    return (
+      <CenterDiv>
+        <PuffLoader color="#fea500" />
+      </CenterDiv>
+    );
+  }
 
   return (
     <CoverContext.Provider value={showCover}>
       <GlobalStyle />
       <SWRConfig value={{ fetcher }}>
-        <Component {...pageProps} />
+        <Component {...pageProps} theme={theme} toggleTheme={toggleTheme} />
       </SWRConfig>
     </CoverContext.Provider>
   );
