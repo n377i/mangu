a9c91caa377e156707a3a43f46c137847bd52fea
Annette Wöhler <annettewoehler@gmx.de>
2024-04-29 11:26:30 +0200

    + Add recipe ID to response

--- pages/api/recipes/index.js
+++ pages/api/recipes/index.js
@@ -10,9 +10,11 @@ export default async function handler(request, response) {
   } else if (request.method === "POST") {
     try {
       const recipeData = request.body;
-      await Recipe.create(recipeData);
+      const createdRecipe = await Recipe.create(recipeData);
 
-      response.status(201).json({ status: "Recipe created" });
+      response
+        .status(201)
+        .json({ _id: createdRecipe._id, status: "Recipe created" });
     } catch (error) {
       console.log(error);
       response.status(400).json({ error: error.message });
