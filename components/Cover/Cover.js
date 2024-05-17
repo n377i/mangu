5c0792d977afcc107276a7fed96f47aaf266153d
Annette Wöhler <annettewoehler@gmx.de>
2024-05-18 01:02:20 +0200

    - Remove animation

--- components/Cover/Cover.js
+++ components/Cover/Cover.js
@@ -1,22 +1,10 @@
 import Logo from "@/public/logo.svg";
-import { useState, useEffect } from "react";
 import { StyledDiv } from "./Cover.styled";
 
 export default function Cover() {
-  const [isLogoVisible, setIsLogoVisible] = useState(false);
-
-  useEffect(() => {
-    setIsLogoVisible(true);
-  }, []);
-
   return (
     <StyledDiv>
-      <Logo
-        style={{
-          transform: isLogoVisible ? "scale(1)" : "scale(0.8)",
-          transition: "transform 1.5s ease-in-out",
-        }}
-      />
+      <Logo />
     </StyledDiv>
   );
 }
