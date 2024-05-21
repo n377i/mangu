9668f2d8ec02e54d7dfbb3d77c613b27bc324e19
Annette Wöhler <annettewoehler@gmx.de>
2024-05-21 18:56:14 +0200

    - Remove theme button

--- components/FormNav/FormNav.js
+++ components/FormNav/FormNav.js
@@ -1,14 +1,11 @@
 import { useRouter } from "next/router";
 import { NavContainer, Nav, FormHeading } from "./FormNav.styles";
 import BackButton from "../BackButton/BackButton";
-import ThemeButton from "../ThemeButton/ThemeButton";
 
-export default function FormNav({ theme, toggleTheme }) {
+export default function FormNav({ theme }) {
   const router = useRouter();
   const isCreate = router.pathname === "/create";
   const headingText = isCreate ? "Rezept hinzufügen" : "Rezept bearbeiten";
-  const iconSrc =
-    theme === "dark" ? "/assets/icon_theme_dark.svg" : "/assets/icon_theme.svg";
 
   return (
     <>
@@ -16,7 +13,6 @@ export default function FormNav({ theme, toggleTheme }) {
         <Nav>
           <BackButton $isDefaultIcon theme={theme} />
           <FormHeading>{headingText}</FormHeading>
-          <ThemeButton toggleTheme={toggleTheme} iconSrc={iconSrc} />
         </Nav>
       </NavContainer>
     </>
