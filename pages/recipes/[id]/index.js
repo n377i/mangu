01a0e87ed30886d0c803c810dde34ce7a7f3c850
Annette Wöhler <annettewoehler@gmx.de>
2024-05-24 21:27:03 +0200

    + Add edit-modal

--- pages/recipes/[id]/index.js
+++ pages/recipes/[id]/index.js
@@ -1,40 +1,75 @@
 import useSWR from "swr";
+import { useState } from "react";
 import { useRouter } from "next/router";
 import { CenterDiv } from "@/styles";
 import { PuffLoader } from "react-spinners";
 import RecipeDetails from "@/components/RecipeDetails/RecipeDetails";
+import Modal from "@/components/Modal/Modal";
+import Form from "@/components/Form/Form";
 
 export default function DetailsPage({ theme }) {
   const router = useRouter();
-  const { isReady } = router;
   const { id } = router.query;
+  const [isEditOpen, setEditOpen] = useState(false);
 
   const {
     data: recipe,
     isLoading,
     error,
+    mutate: mutateRecipe,
   } = useSWR(id ? `/api/recipes/${id}` : null);
 
-  if (!isReady || isLoading || error)
+  async function deleteRecipe() {
+    await fetch(`/api/recipes/${id}`, { method: "DELETE" });
+    router.push("/");
+  }
+
+  async function editRecipe(updatedRecipe) {
+    try {
+      await fetch(`/api/recipes/${id}`, {
+        method: "PATCH",
+        body: JSON.stringify(updatedRecipe),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      mutateRecipe();
+      console.log("Recipe edited successfully.");
+      setEditOpen(false);
+    } catch (error) {
+      console.error("Error editing recipe:", error);
+    }
+  }
+
+  if (isLoading || error) {
     return (
       <CenterDiv>
         <PuffLoader color="#fea500" />
       </CenterDiv>
     );
-
-  async function deleteRecipe() {
-    await fetch(`/api/recipes/${id}`, {
-      method: "DELETE",
-    });
-    router.push("/");
   }
 
   return (
-    <RecipeDetails
-      recipe={recipe}
-      id={id}
-      deleteRecipe={deleteRecipe}
-      theme={theme}
-    />
+    <>
+      <RecipeDetails
+        recipe={recipe}
+        id={id}
+        deleteRecipe={deleteRecipe}
+        theme={theme}
+        onEdit={() => setEditOpen(true)}
+      />
+      <Modal
+        modalTitle="Rezept bearbeiten"
+        isOpen={isEditOpen}
+        onClose={() => setEditOpen(false)}
+      >
+        <Form
+          onSubmit={editRecipe}
+          onClose={() => setEditOpen(false)}
+          formName={"edit-recipe"}
+          defaultData={recipe}
+          theme={theme}
+        />
+      </Modal>
+    </>
   );
 }
