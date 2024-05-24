dd8069cccf33544fa65fbe05fc7539a258281d4d
Annette Wöhler <annettewoehler@gmx.de>
2024-05-24 21:26:45 +0200

    + Add create-modal

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,23 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CoverContext } from "@/pages/_app";
 import RecipeList from "@/components/RecipeList/RecipeList";
 import BottomNav from "@/components/BottomNav/BottomNav";
 import TopNav from "@/components/TopNav/TopNav";
+import Modal from "@/components/Modal/Modal";
+import Form from "@/components/Form/Form";
 import styled from "styled-components";
 import useLocalStorage from "use-local-storage";
+import { useRouter } from "next/router";
 
 const MainContent = styled.main`
-  padding: 91px 0 100px;
+  padding: 95px 0 100px;
 `;
 
 export default function HomePage({ theme, toggleTheme }) {
   const showCover = useContext(CoverContext);
   const [gridColumns, setGridColumns] = useLocalStorage("gridColumns", 2);
+  const [isCreateOpen, setCreateOpen] = useState(false);
+  const router = useRouter();
 
   const toggleGridColumns = () => {
     if (gridColumns === 2) {
@@ -21,6 +26,23 @@ export default function HomePage({ theme, toggleTheme }) {
       setGridColumns(1);
     } else {
       setGridColumns(2);
+    }
+  };
+
+  const addRecipe = async (recipe) => {
+    const response = await fetch("/api/recipes", {
+      method: "POST",
+      body: JSON.stringify(recipe),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      const data = await response.json();
+      const { _id } = data;
+      router.push(`/recipes/${_id}`);
+    } else {
+      console.error(response.status);
     }
   };
 
@@ -36,7 +58,19 @@ export default function HomePage({ theme, toggleTheme }) {
           <MainContent>
             <RecipeList gridColumns={gridColumns} />
           </MainContent>
-          <BottomNav theme={theme} />
+          <BottomNav theme={theme} onAddClick={() => setCreateOpen(true)} />
+          <Modal
+            modalTitle="Rezept erstellen"
+            isOpen={isCreateOpen}
+            onClose={() => setCreateOpen(false)}
+          >
+            <Form
+              onSubmit={addRecipe}
+              onClose={() => setCreateOpen(false)}
+              formName={"add-recipe"}
+              theme={theme}
+            />
+          </Modal>
         </>
       )}
     </>
