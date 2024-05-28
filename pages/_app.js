ae080bd74cf341dd98354df002bf827c435f99da
Annette Wöhler <annettewoehler@gmx.de>
2024-05-28 17:40:01 +0200

    - Remove cover

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,15 @@
-import { useState, useEffect, createContext } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { SWRConfig } from "swr";
-import { PuffLoader } from "react-spinners";
-import { CenterDiv } from "@/styles";
 import GlobalStyle from "../styles";
 import useLocalStorage from "use-local-storage";
-import Cover from "@/components/Cover/Cover";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
-
-export const CoverContext = createContext();
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const [themeLoaded, setThemeLoaded] = useState(false);
   const [theme, setTheme] = useLocalStorage("theme", "light");
-  const [showCover, setShowCover] = useState(true);
 
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
@@ -41,29 +35,12 @@ export default function MyApp({ Component, pageProps }) {
     }
   }, [theme]);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowCover(false);
-    }, 800);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (!themeLoaded) {
-    return (
-      <CenterDiv>
-        <PuffLoader color="#fea500" />
-      </CenterDiv>
-    );
-  }
-
   return (
-    <CoverContext.Provider value={showCover}>
+    <>
       <GlobalStyle />
-      {showCover && <Cover />}
       <SWRConfig value={{ fetcher }}>
         <Component {...pageProps} theme={theme} toggleTheme={toggleTheme} />
       </SWRConfig>
-    </CoverContext.Provider>
+    </>
   );
 }
